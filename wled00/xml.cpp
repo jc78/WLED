4202753f12e6833abc6a30900f64af0edf98ac3c
Jason Childress <jasonchildress.dev@gmail.com>
2023-09-16 18:59:23 -0700

    converting pro firmware to regular

--- wled00/xml.cpp
+++ wled00/xml.cpp
@@ -149,7 +149,6 @@ void appendGPIOinfo() {
   oappend(SET_F("d.rsvd=[11,12,13,14,15,16,17"));
   #elif defined(ESP32)
   oappend(SET_F("d.rsvd=[6,7,8,9,10,11,24,28,29,30,31,37,38"));
-  // oappend(SET_F("d.rsvd=[0,1,2,3,4,5,6,7,8,9,10,11,12,15,16,17,18,19,20,21,22,24,28,29,30,31,37,38"));
   #else
   oappend(SET_F("d.rsvd=[6,7,8,9,10,11"));
   #endif
